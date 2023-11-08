3f5d01f274c2cb8f419aec093465aa98a2c9b959
jkimball54 <jeremyck54@gmail.com>
2023-11-08 15:20:20 -0700

    added data access and context files

---
+++ DeveloperDashboard/DataAccess/DeveloperDashboardContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DeveloperDashboard.Models;
+using System.Collections.Generic;
+
+namespace DeveloperDashboard.DataAccess
+{
+    public class DeveloperDashboardContext : DbContext
+    {
+        public DeveloperDashboardContext(DbContextOptions<DeveloperDashboardContext> options) : base(options)
+        {
+
+        }
+    }
+}
