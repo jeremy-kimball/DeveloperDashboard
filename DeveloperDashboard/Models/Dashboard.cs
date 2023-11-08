c512166085e4945cad3ad2d144ecff2cb7e94c6c
jkimball54 <jeremyck54@gmail.com>
2023-11-08 15:27:07 -0700

    added dashboard and widget models

---
+++ DeveloperDashboard/Models/Dashboard.cs
@@ -0,0 +1,9 @@
+﻿namespace DeveloperDashboard.Models
+{
+    public class Dashboard
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Widget> Widgets { get; set; }
+    }
+}
---
+++ DeveloperDashboard/Models/Widget.cs
@@ -0,0 +1,11 @@
+﻿namespace DeveloperDashboard.Models
+{
+    public class Widget
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Content { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+    }
+}
