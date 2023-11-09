5fce096746dbc74c8840c5c383a9e19624fdad07
jkimball54 <jeremyck54@gmail.com>
2023-11-08 18:11:00 -0700

    removed un-needed model

--- DeveloperDashboard/Models/DashboardViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DeveloperDashboard.Models
-{
-    public class DashboardViewModel
-    {
-        public int[] SelectedWidgetIds { get; set; }
-        public IEnumerable<Widget> AvailableWidgets { get; set; }
-    }
-}
