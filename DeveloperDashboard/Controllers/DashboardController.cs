07835f525d8c1de242f715da708fa0078a384f8f
jkimball54 <jeremyck54@gmail.com>
2023-11-09 13:55:09 -0700

    Index, update, edit finished, new changed to checkbox for form

--- DeveloperDashboard/Controllers/DashboardController.cs
+++ DeveloperDashboard/Controllers/DashboardController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using DeveloperDashboard.Models;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using System.Xml.Linq;
 
 namespace DeveloperDashboard.Controllers
 {
@@ -17,13 +18,15 @@ namespace DeveloperDashboard.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            var dashboards = _context.Dashboards.ToList();
+            return View(dashboards);
         }
 
         public IActionResult New()
         {
-            var widgetList = _context.Widgets;
-            ViewBag.widgetBag = new SelectList(widgetList, "Id", "Name");
+            var widgetList = _context.Widgets.ToList();
+            ViewBag.widgetBag = widgetList;
+
             return View();
         }
 
@@ -37,19 +40,10 @@ namespace DeveloperDashboard.Controllers
         [HttpPost]
         public async Task<IActionResult> Create(string name, int[] selectedWidgetIds)
         {
-            if (selectedWidgetIds.Length == 0)
-            {
-                ModelState.AddModelError("", "You must select at least one widget.");
-            }
-            if (string.IsNullOrWhiteSpace(name))
-            {
-                ModelState.AddModelError("Name", "The Name field is required.");
-            }
 
             if (!ModelState.IsValid)
             {
-                ViewBag.widgetBag = new SelectList(_context.Widgets, "Id", "Name");
-                return View();
+                return BadRequest();
             }
 
             var selectedWidgets = _context.Widgets
@@ -63,9 +57,50 @@ namespace DeveloperDashboard.Controllers
             };
 
             _context.Dashboards.Add(dashboard);
-            await _context.SaveChangesAsync();
+            _context.SaveChanges();
 
             return Redirect($"/Dashboard/{dashboard.Id}");
+        }
+
+        [Route("/Dashboard/{id:int}/Edit")]
+        public IActionResult Edit(int id)
+        {
+            var dashboard = _context.Dashboards.Include(d => d.Widgets).First(d => d.Id == id);
+            var widgetList = _context.Widgets.ToList();
+            var dashboardWidgets = dashboard.Widgets.Select(w => w.Id).ToList();
+
+            ViewBag.widgetBag = widgetList;
+            ViewBag.dashboardWidgetIds = dashboardWidgets;
+
+            return View(dashboard);
+        }
+
+        [HttpPost]
+        [Route("/Dashboard/{id:int}/Update")]
+        public IActionResult Update(int id, Dashboard dashboard, int[] selectedWidgetIds)
+        {
+            var dashboardToUpdate = _context.Dashboards.Include(d => d.Widgets).First(d => d.Id == id);
+            dashboardToUpdate.Name = dashboard.Name;
+            dashboardToUpdate.Widgets.Clear();
+            foreach(var widgetId in selectedWidgetIds)
+            {
+                dashboardToUpdate.Widgets.Add(_context.Widgets.Find(widgetId));
+            }
+            _context.Dashboards.Update(dashboardToUpdate);
+            _context.SaveChanges();
+
+            return Redirect($"/Dashboard/{dashboardToUpdate.Id}");
+        }
+
+        [HttpPost]
+        [Route("/Dashboard/{id:int}/Delete")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var dashboardToDelete = _context.Dashboards.Include(d => d.Widgets).First(d => d.Id == id);
+            _context.Dashboards.Remove(dashboardToDelete);
+            _context.SaveChanges();
+
+            return Redirect($"/Dashboard");
         }
     }
 }
