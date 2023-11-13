f2a107e926d94c34649f3bf5b9f4272944c1ede8
jkimball54 <jeremyck54@gmail.com>
2023-11-13 11:12:09 -0700

    added migration

---
+++ DeveloperDashboard/Migrations/20231113181053_googleSearch.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DeveloperDashboard.Migrations
+{
+    /// <inheritdoc />
+    public partial class googleSearch : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "widgets",
+                columns: new[] { "id", "content", "height", "name", "width" },
+                values: new object[] { 2, "_GoogleSearch", 2, "Google Search", 2 });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "widgets",
+                keyColumn: "id",
+                keyValue: 2);
+        }
+    }
+}
