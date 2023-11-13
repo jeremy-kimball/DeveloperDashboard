200f0889c2c60d2fb515dcc19c589c2d7c35592b
jkimball54 <jeremyck54@gmail.com>
2023-11-13 13:28:00 -0700

    added properties to response models

--- DeveloperDashboard/Models/DailyWeather.cs
+++ DeveloperDashboard/Models/DailyWeather.cs
@@ -2,5 +2,9 @@
 {
     public class DailyWeather
     {
+        public string Datetime { get; set; }
+        public double Tempmax { get; set; }
+        public double Tempmin { get; set; }
+        public double Temp { get; set; }
     }
 }
--- DeveloperDashboard/Models/WeatherApiResponse.cs
+++ DeveloperDashboard/Models/WeatherApiResponse.cs
@@ -2,5 +2,12 @@
 {
     public class WeatherApiResponse
     {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string ResolvedAddress { get; set; }
+        public string Address { get; set; }
+        public string Timezone { get; set; }
+        public double Tzoffset { get; set; }
+        public List<DailyWeather> Days { get; set; }
     }
 }
