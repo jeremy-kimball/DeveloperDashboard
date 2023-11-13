cc29b3e1ca8d9da091302432cb30792c7286e297
jkimball54 <jeremyck54@gmail.com>
2023-11-13 13:46:16 -0700

    add migration for weather widget

---
+++ DeveloperDashboard/Migrations/20231113204441_addWeatherWidget.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DeveloperDashboard.Migrations
+{
+    /// <inheritdoc />
+    public partial class addWeatherWidget : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "widgets",
+                columns: new[] { "id", "content", "height", "name", "width" },
+                values: new object[] { 3, "_Weather", 2, "Weather", 2 });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "widgets",
+                keyColumn: "id",
+                keyValue: 3);
+        }
+    }
+}
