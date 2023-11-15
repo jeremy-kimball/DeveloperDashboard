1a9b95a9a4d5679f1946a6786387e5e1781e8585
jkimball54 <jeremyck54@gmail.com>
2023-11-15 14:13:15 -0700

    sucessfully mocking UserManager

--- DeveloperDashboard.Testing/HomeControllerTests.cs
+++ DeveloperDashboard.Testing/HomeControllerTests.cs
@@ -4,17 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DeveloperDashboard.DataAccess;
+using DeveloperDashboard.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
+using Moq;
 
 namespace DeveloperDashboard.Testing
 {
     public class HomeControllerTests : IClassFixture<WebApplicationFactory<Program>>
     {
         private readonly WebApplicationFactory<Program> _factory;
+        private readonly Mock<UserManager<ApplicationUser>> _mockUserManager;
 
         public HomeControllerTests(WebApplicationFactory<Program> factory)
         {
+            _mockUserManager = new Mock<UserManager<ApplicationUser>>(
+            new Mock<IUserStore<ApplicationUser>>().Object,
+            null, null, null, null, null, null, null, null);
             _factory = factory;
         }
 
