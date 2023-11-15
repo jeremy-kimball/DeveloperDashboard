fda092f09b9d28da967ba9f2ffcaa06f17598f2f
jkimball54 <jeremyck54@gmail.com>
2023-11-15 14:14:23 -0700

    add UserManager mock to DashboardControllerTests

--- DeveloperDashboard.Testing/DashboardControllerTests.cs
+++ DeveloperDashboard.Testing/DashboardControllerTests.cs
@@ -4,17 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DeveloperDashboard.DataAccess;
+using DeveloperDashboard.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
+using Moq;
 
 namespace DeveloperDashboard.Testing
 {
     public class DashboardControllerTests : IClassFixture<WebApplicationFactory<Program>>
     {
         private readonly WebApplicationFactory<Program> _factory;
+        private readonly Mock<UserManager<ApplicationUser>> _mockUserManager;
 
         public DashboardControllerTests(WebApplicationFactory<Program> factory)
         {
+            _mockUserManager = new Mock<UserManager<ApplicationUser>>(
+            new Mock<IUserStore<ApplicationUser>>().Object,
+            null, null, null, null, null, null, null, null);
             _factory = factory;
         }
 
