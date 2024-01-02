c0ab268a450016d412748887903a2e94a03e9ab6
jkimball54 <jeremyck54@gmail.com>
2024-01-02 15:10:30 -0700

    Migrations

--- DeveloperDashboard/Program.cs
+++ DeveloperDashboard/Program.cs
@@ -26,6 +26,13 @@ builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.R
     .AddEntityFrameworkStores<DeveloperDashboardContext>();
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<DeveloperDashboardContext>();
+    // This will throw an exception if the connection fails
+    dbContext.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
