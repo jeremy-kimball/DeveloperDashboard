b520d9c0a7687bea10bcca642a91f53f400d752b
jkimball54 <jeremyck54@gmail.com>
2024-01-02 14:14:45 -0700

    syntax experimentation

--- DeveloperDashboard/Program.cs
+++ DeveloperDashboard/Program.cs
@@ -3,27 +3,25 @@ using DeveloperDashboard.DataAccess;
 using DeveloperDashboard.Services;
 using Microsoft.AspNetCore.Identity;
 using DeveloperDashboard.Models;
-using Npgsql;
 
 var builder = WebApplication.CreateBuilder(args);
+string DEVELOPERDASHBOARD_DBCONNECTIONSTRING = $"Server={Environment.GetEnvironmentVariable("PGHOST")};Database={Environment.GetEnvironmentVariable("PGDATABASE")};Port={Environment.GetEnvironmentVariable("PGPORT")};Username={Environment.GetEnvironmentVariable("PGUSER")};Password={Environment.GetEnvironmentVariable("PGPASSWORD")}";
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddSingleton<IUrlShortenerApiService, UrlShortenerApiService>();
 builder.Services.AddSingleton<IWeatherApiService, WeatherApiService>();
-
-var postgresConnectionString = new NpgsqlConnectionStringBuilder
-{
-    Host = Environment.GetEnvironmentVariable("PGHOST") ?? throw new InvalidOperationException("Database host (PGHOST) not found."),
-    Database = Environment.GetEnvironmentVariable("PGDATABASE") ?? throw new InvalidOperationException("Database name (PGDATABASE) not found."),
-    Port = int.Parse(Environment.GetEnvironmentVariable("PGPORT") ?? throw new InvalidOperationException("Database port (PGPORT) not found.")),
-    Username = Environment.GetEnvironmentVariable("PGUSER") ?? throw new InvalidOperationException("Database username (PGUSER) not found."),
-    Password = Environment.GetEnvironmentVariable("PGPASSWORD") ?? throw new InvalidOperationException("Database password (PGPASSWORD) not found.")
-}.ConnectionString;
-
 builder.Services.AddDbContext<DeveloperDashboardContext>(
-    options => options.UseNpgsql(postgresConnectionString).UseSnakeCaseNamingConvention()
-);
+    options =>
+        options
+            .UseNpgsql(
+                builder.Configuration[DEVELOPERDASHBOARD_DBCONNECTIONSTRING]
+                    ?? throw new InvalidOperationException(
+                            "Connection String 'DevDashDBNotFound' not found"
+                            )
+                    )
+                    .UseSnakeCaseNamingConvention()
+                    );
 
 builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<DeveloperDashboardContext>();
